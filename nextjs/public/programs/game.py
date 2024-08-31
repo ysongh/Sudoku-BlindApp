67f0aad5bc5b666a12c446d64ba649e44a3c47be
You Song Hou <ysongweb3@gmail.com>
2024-08-30 21:52:13 -0400

    feat: hard-code the values for the array

--- nextjs/public/programs/game.py
+++ nextjs/public/programs/game.py
@@ -4,16 +4,15 @@ def nada_main():
 
     party1 = Party(name="Party1")
 
-    grid: list[SecretInteger] = []
+    grid: list[SecretInteger] = [Integer(1), Integer(3), Integer(2), Integer(4)]
 
     my_int1 = SecretInteger(Input(name="my_int1", party=party1))
 
     my_int2 = SecretInteger(Input(name="my_int2", party=party1))
 
-    grid.append(Integer(9))
     grid.append(my_int2 + my_int1)
 
     return [
         Output(grid[i], "my_output" + str(i), party1)
-        for i in range(2)
+        for i in range(5)
     ]
--- nextjs/src/pages/_app.tsx
+++ nextjs/src/pages/_app.tsx
@@ -11,17 +11,17 @@ import type { AppProps } from "next/app";
 import Navbar from "@/components/Navbar";
 
 const client = NillionClient.create({
-  network: NamedNetwork.enum.Photon,
+  network: NamedNetwork.enum.Devnet,
   overrides: async () => {
     const signer = await createSignerFromKey(
-      process.env.NEXT_PUBLIC_REACT_APP_NILLION_NILCHAIN_PRIVATE_KEY!,
+      process.env.NEXT_PUBLIC_NILLION_NILCHAIN_PRIVATE_KEY_0!,
     );
     return {
       endpoint: "http://localhost:8080/nilchain",
       signer,
       userSeed: "example@nillion",
-      cluster: process.env.NEXT_PUBLIC_REACT_APP_NILLION_CLUSTER_ID,
-      bootnodes: [process.env.NEXT_PUBLIC_REACT_APP_NILLION_BOOTNODE_WEBSOCKET],
+      cluster: process.env.NEXT_PUBLIC_NILLION_CLUSTER_ID,
+      bootnodes: [process.env.NEXT_PUBLIC_NILLION_BOOTNODE_WEBSOCKET],
     };
   },
 });
