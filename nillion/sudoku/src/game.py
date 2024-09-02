df8e729f728e6fe19e8d892c6f19dc8f713ce8ae
You Song Hou <ysongweb3@gmail.com>
2024-09-01 22:05:40 -0400

    feat: add a function to check the value at a position

--- nillion/sudoku/src/game.py
+++ nillion/sudoku/src/game.py
@@ -1,19 +1,27 @@
 from nada_dsl import *
 
+def check_value_at_position(position_value: SecretInteger, target_secret_value: Integer) -> Boolean:
+    return position_value == target_secret_value
+
 def nada_main():
 
     party1 = Party(name="Party1")
 
     grid: list[SecretInteger] = [Integer(1), Integer(3), Integer(2), Integer(4)]
 
+    size: 4
+
     my_int1 = SecretInteger(Input(name="my_int1", party=party1))
 
     my_int2 = SecretInteger(Input(name="my_int2", party=party1))
 
-    grid.append(my_int1 + my_int2)
-    grid.append(SecretInteger.random() % Integer(10));
+    for i in range(4):
+        is_target = check_value_at_position(my_int1, Integer(i))
+        grid[i] = is_target.if_else(my_int2, grid[i])
+
+    # grid.append(SecretInteger.random() % Integer(10));
 
     return [
         Output(grid[i], str(i), party1)
-        for i in range(6)
+        for i in range(4)
     ]
