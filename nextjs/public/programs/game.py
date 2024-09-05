723769b6de703a765fa4482a62fed4c837813c9f
You Song Hou <ysongweb3@gmail.com>
2024-09-05 00:55:02 -0400

    feat: expand the array

--- nextjs/public/programs/game.py
+++ nextjs/public/programs/game.py
@@ -8,35 +8,142 @@ def nada_main():
     party1 = Party(name="Party1")
     # party2 = Party(name="Party2")
 
-    answerGrid: list[SecretInteger] = [Integer(1), Integer(3), Integer(2), Integer(4)]
-    playerGrid: list[SecretInteger] = [Integer(1), Integer(3), Integer(2), Integer(4)]
+    answerGrid: list[SecretInteger] = [
+        # Row 1
+        Integer(1),
+        Integer(4),
+        Integer(2),
+        Integer(3),
+
+        # Row 2
+        Integer(2),
+        Integer(3),
+        Integer(1),
+        Integer(4),
+
+        # Row 3
+        Integer(3),
+        Integer(1),
+        Integer(4),
+        Integer(2),
+
+        # Row 4
+        Integer(4),
+        Integer(2),
+        Integer(3),
+        Integer(1)
+    ]
+    playerGrid: list[SecretInteger] = [
+        # Row 1
+        Integer(1),
+        Integer(4),
+        Integer(2),
+        Integer(3),
+
+        # Row 2
+        Integer(2),
+        Integer(3),
+        Integer(1),
+        Integer(4),
+
+        # Row 3
+        Integer(3),
+        Integer(1),
+        Integer(4),
+        Integer(2),
+
+        # Row 4
+        Integer(4),
+        Integer(2),
+        Integer(3),
+        Integer(1)
+    ]
 
     my_int1 = SecretInteger(Input(name="my_int1", party=party1))
     my_int2 = SecretInteger(Input(name="my_int2", party=party1))
+    my_int3 = SecretInteger(Input(name="my_int3", party=party1))
+    my_int4 = SecretInteger(Input(name="my_int4", party=party1))
+    my_int5 = SecretInteger(Input(name="my_int5", party=party1))
+    my_int6 = SecretInteger(Input(name="my_int6", party=party1))
+    my_int7 = SecretInteger(Input(name="my_int7", party=party1))
+    my_int8 = SecretInteger(Input(name="my_int8", party=party1))
+    my_int9 = SecretInteger(Input(name="my_int9", party=party1))
+    my_int10 = SecretInteger(Input(name="my_int10", party=party1))
 
-    size = 4
+    size = 16
 
     player_target_1 = SecretInteger(Input(name="player_target_1", party=party1))
-    player_input_1 = SecretInteger(Input(name="player_input_1", party=party1))
     player_target_2 = SecretInteger(Input(name="player_target_2", party=party1))
+    player_target_3 = SecretInteger(Input(name="player_target_3", party=party1))
+    player_target_4 = SecretInteger(Input(name="player_target_4", party=party1))
+    player_target_5 = SecretInteger(Input(name="player_target_5", party=party1))
+    player_target_6 = SecretInteger(Input(name="player_target_6", party=party1))
+    player_target_7 = SecretInteger(Input(name="player_target_7", party=party1))
+    player_target_7 = SecretInteger(Input(name="player_target_7", party=party1))
+    player_target_8 = SecretInteger(Input(name="player_target_8", party=party1))
+    player_target_9 = SecretInteger(Input(name="player_target_9", party=party1))
+    player_target_10 = SecretInteger(Input(name="player_target_10", party=party1))
+    
+    player_input_1 = SecretInteger(Input(name="player_input_1", party=party1))
     player_input_2 = SecretInteger(Input(name="player_input_2", party=party1))
+    player_input_3 = SecretInteger(Input(name="player_input_3", party=party1))
+    player_input_4 = SecretInteger(Input(name="player_input_4", party=party1))
+    player_input_5 = SecretInteger(Input(name="player_input_5", party=party1))
+    player_input_6 = SecretInteger(Input(name="player_input_6", party=party1))
+    player_input_7 = SecretInteger(Input(name="player_input_7", party=party1))
+    player_input_8 = SecretInteger(Input(name="player_input_8", party=party1))
+    player_input_9 = SecretInteger(Input(name="player_input_9", party=party1))
+    player_input_10 = SecretInteger(Input(name="player_input_10", party=party1))
 
     for i in range(size):
         is_target_1 = check_value_at_position(my_int1, Integer(i))
         is_target_2 = check_value_at_position(my_int2, Integer(i))
+        is_target_3 = check_value_at_position(my_int3, Integer(i))
+        is_target_4 = check_value_at_position(my_int4, Integer(i))
+        is_target_5 = check_value_at_position(my_int5, Integer(i))
+        is_target_6 = check_value_at_position(my_int6, Integer(i))
+        is_target_7 = check_value_at_position(my_int7, Integer(i))
+        is_target_8 = check_value_at_position(my_int8, Integer(i))
+        is_target_9 = check_value_at_position(my_int9, Integer(i))
+        is_target_10 = check_value_at_position(my_int10, Integer(i))
+
         playerGrid[i] = is_target_1.if_else(Integer(0), playerGrid[i])
         playerGrid[i] = is_target_2.if_else(Integer(0), playerGrid[i])
+        playerGrid[i] = is_target_3.if_else(Integer(0), playerGrid[i])
+        playerGrid[i] = is_target_4.if_else(Integer(0), playerGrid[i])
+        playerGrid[i] = is_target_5.if_else(Integer(0), playerGrid[i])
+        playerGrid[i] = is_target_6.if_else(Integer(0), playerGrid[i])
+        playerGrid[i] = is_target_7.if_else(Integer(0), playerGrid[i])
+        playerGrid[i] = is_target_8.if_else(Integer(0), playerGrid[i])
+        playerGrid[i] = is_target_9.if_else(Integer(0), playerGrid[i])
+        playerGrid[i] = is_target_10.if_else(Integer(0), playerGrid[i])
 
-    
     for i in range(size):
         is_target_1 = check_value_at_position(player_target_1, Integer(i))
         is_target_2 = check_value_at_position(player_target_2, Integer(i))
+        is_target_3 = check_value_at_position(player_target_3, Integer(i))
+        is_target_4 = check_value_at_position(player_target_4, Integer(i))
+        is_target_5 = check_value_at_position(player_target_5, Integer(i))
+        is_target_6 = check_value_at_position(player_target_6, Integer(i))
+        is_target_7 = check_value_at_position(player_target_7, Integer(i))
+        is_target_8 = check_value_at_position(player_target_8, Integer(i))
+        is_target_9 = check_value_at_position(player_target_9, Integer(i))
+        is_target_10 = check_value_at_position(player_target_10, Integer(i))
+
         playerGrid[i] = is_target_1.if_else(player_input_1, playerGrid[i])
         playerGrid[i] = is_target_2.if_else(player_input_2, playerGrid[i])
+        playerGrid[i] = is_target_3.if_else(player_input_3, playerGrid[i])
+        playerGrid[i] = is_target_4.if_else(player_input_4, playerGrid[i])
+        playerGrid[i] = is_target_5.if_else(player_input_5, playerGrid[i])
+        playerGrid[i] = is_target_6.if_else(player_input_6, playerGrid[i])
+        playerGrid[i] = is_target_7.if_else(player_input_7, playerGrid[i])
+        playerGrid[i] = is_target_8.if_else(player_input_8, playerGrid[i])
+        playerGrid[i] = is_target_9.if_else(player_input_9, playerGrid[i])
+        playerGrid[i] = is_target_10.if_else(player_input_10, playerGrid[i])
 
     # grid.append(SecretInteger.random() % Integer(10));
 
     return [
         Output(playerGrid[i], str(i), party1)
-        for i in range(4)
+        for i in range(size)
     ]
--- nextjs/src/pages/game/index.tsx
+++ nextjs/src/pages/game/index.tsx
@@ -30,12 +30,39 @@ export default function Compute() {
 
   // UseStates
   const [selectedProgramCode, setSelectedProgramCode] = useState("");
-  const [secretValue1, setSecretValue1] = useState<number>(0);
-  const [secretValue2, setSecretValue2] = useState<number>(0);
-  const [secretValue3, setSecretValue3] = useState<number>(0);
-  const [secretValue4, setSecretValue4] = useState<number>(0);
-  const [secretValue5, setSecretValue5] = useState<number>(0);
-  const [secretValue6, setSecretValue6] = useState<number>(0);
+  const [secretValue1, setSecretValue1] = useState<number>(1);
+  const [secretValue2, setSecretValue2] = useState<number>(4);
+  const [secretValue3, setSecretValue3] = useState<number>(5);
+  const [secretValue4, setSecretValue4] = useState<number>(6);
+  const [secretValue5, setSecretValue5] = useState<number>(7);
+  const [secretValue6, setSecretValue6] = useState<number>(8);
+  const [secretValue7, setSecretValue7] = useState<number>(9);
+  const [secretValue8, setSecretValue8] = useState<number>(11);
+  const [secretValue9, setSecretValue9] = useState<number>(13);
+  const [secretValue10, setSecretValue10] = useState<number>(14);
+
+  const [targetNumber1, setTargetNumber1] = useState<number>(0);
+  const [targetNumber2, setTargetNumber2] = useState<number>(0);
+  const [targetNumber3, setTargetNumber3] = useState<number>(0);
+  const [targetNumber4, setTargetNumber4] = useState<number>(0);
+  const [targetNumber5, setTargetNumber5] = useState<number>(0);
+  const [targetNumber6, setTargetNumber6] = useState<number>(0);
+  const [targetNumber7, setTargetNumber7] = useState<number>(0);
+  const [targetNumber8, setTargetNumber8] = useState<number>(0);
+  const [targetNumber9, setTargetNumber9] = useState<number>(0);
+  const [targetNumber10, setTargetNumber10] = useState<number>(0);
+
+  const [targetValue1, setTargetValue1] = useState<number>(0);
+  const [targetValue2, setTargetValue2] = useState<number>(0);
+  const [targetValue3, setTargetValue3] = useState<number>(0);
+  const [targetValue4, setTargetValue4] = useState<number>(0);
+  const [targetValue5, setTargetValue5] = useState<number>(0);
+  const [targetValue6, setTargetValue6] = useState<number>(0);
+  const [targetValue7, setTargetValue7] = useState<number>(0);
+  const [targetValue8, setTargetValue8] = useState<number>(0);
+  const [targetValue9, setTargetValue9] = useState<number>(0);
+  const [targetValue10, setTargetValue10] = useState<number>(0);
+
   const [programID, setProgramID] = useState<ProgramId>();
   const [secretValue1ID, setSecretValue1ID] = useState<StoreId>();
   const [secretValue2ID, setSecretValue2ID] = useState<StoreId>();
@@ -79,20 +106,118 @@ export default function Compute() {
           NadaValue.createSecretInteger(secretValue2)
         )
         .insert(
-          NamedValue.parse("player_target_1"),
+          NamedValue.parse("my_int3"),
           NadaValue.createSecretInteger(secretValue3)
         )
         .insert(
-          NamedValue.parse("player_input_1"),
+          NamedValue.parse("my_int4"),
           NadaValue.createSecretInteger(secretValue4)
         )
         .insert(
-          NamedValue.parse("player_target_2"),
+          NamedValue.parse("my_int5"),
           NadaValue.createSecretInteger(secretValue5)
         )
         .insert(
-          NamedValue.parse("player_input_2"),
+          NamedValue.parse("my_int6"),
           NadaValue.createSecretInteger(secretValue6)
+        )
+        .insert(
+          NamedValue.parse("my_int7"),
+          NadaValue.createSecretInteger(secretValue7)
+        )
+        .insert(
+          NamedValue.parse("my_int8"),
+          NadaValue.createSecretInteger(secretValue8)
+        )
+        .insert(
+          NamedValue.parse("my_int9"),
+          NadaValue.createSecretInteger(secretValue9)
+        )
+        .insert(
+          NamedValue.parse("my_int10"),
+          NadaValue.createSecretInteger(secretValue10)
+        )
+
+        .insert(
+          NamedValue.parse("player_target_1"),
+          NadaValue.createSecretInteger(targetNumber1)
+        )
+        .insert(
+          NamedValue.parse("player_target_2"),
+          NadaValue.createSecretInteger(targetNumber2)
+        )
+        .insert(
+          NamedValue.parse("player_target_3"),
+          NadaValue.createSecretInteger(targetNumber3)
+        )
+        .insert(
+          NamedValue.parse("player_target_4"),
+          NadaValue.createSecretInteger(targetNumber4)
+        )
+        .insert(
+          NamedValue.parse("player_target_5"),
+          NadaValue.createSecretInteger(targetNumber5)
+        )
+        .insert(
+          NamedValue.parse("player_target_6"),
+          NadaValue.createSecretInteger(targetNumber6)
+        )
+        .insert(
+          NamedValue.parse("player_target_7"),
+          NadaValue.createSecretInteger(targetNumber7)
+        )
+        .insert(
+          NamedValue.parse("player_target_8"),
+          NadaValue.createSecretInteger(targetNumber8)
+        )
+        .insert(
+          NamedValue.parse("player_target_9"),
+          NadaValue.createSecretInteger(targetNumber9)
+        )
+        .insert(
+          NamedValue.parse("player_target_10"),
+          NadaValue.createSecretInteger(targetNumber10)
+        )
+
+        .insert(
+          NamedValue.parse("player_input_1"),
+          NadaValue.createSecretInteger(targetValue1)
+        )
+        .insert(
+          NamedValue.parse("player_input_2"),
+          NadaValue.createSecretInteger(targetValue2)
+        )
+        .insert(
+          NamedValue.parse("player_input_3"),
+          NadaValue.createSecretInteger(targetValue3)
+        )
+        .insert(
+          NamedValue.parse("player_input_4"),
+          NadaValue.createSecretInteger(targetValue4)
+        )
+        .insert(
+          NamedValue.parse("player_input_5"),
+          NadaValue.createSecretInteger(targetValue5)
+        )
+        .insert(
+          NamedValue.parse("player_input_6"),
+          NadaValue.createSecretInteger(targetValue6)
+        )
+        .insert(
+          NamedValue.parse("player_input_7"),
+          NadaValue.createSecretInteger(targetValue7)
+        )
+        .insert(
+          NamedValue.parse("player_input_8"),
+          NadaValue.createSecretInteger(targetValue8)
+        )
+        .insert(
+          NamedValue.parse("player_input_9"),
+          NadaValue.createSecretInteger(targetValue9)
+        )
+        .insert(
+          NamedValue.parse("player_input_10"),
+          NadaValue.createSecretInteger(targetValue10)
         );
         
 
@@ -302,52 +427,103 @@ export default function Compute() {
         <Heading my="2">Inputs</Heading>
         <Input
           placeholder="Grid Target 1"
-          value={secretValue3}
-          onChange={(e) => setSecretValue3(Number(e.target.value))}
+          value={targetNumber1}
+          onChange={(e) => setTargetNumber1(Number(e.target.value))}
         />
-        <Button
-          onClick={() => handleStoreSecretInteger3()}
-          className="bg-blue-500 mb-4 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out mt-2"
-        >
-          Store Secret
-        </Button>
-
-        {secretValue3ID && (
-          <div className="mt-2">
-            <p className="text-sm text-gray-600">
-              Secret Value 3 ID: {secretValue3ID}
-            </p>
-          </div>
-        )}
         <Input
           placeholder="Grid Value 1"
-          value={secretValue4}
-          onChange={(e) => setSecretValue4(Number(e.target.value))}
+          value={targetValue1}
+          onChange={(e) => setTargetValue1(Number(e.target.value))}
         />
-         <Button
-          onClick={() => handleStoreSecretInteger4()}
-          className="bg-blue-500 mb-4 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out mt-2"
-        >
-          Store Secret
-        </Button>
-
-        {secretValue4ID && (
-          <div className="mt-2">
-            <p className="text-sm text-gray-600">
-              Secret Value 4 ID: {secretValue4ID}
-            </p>
-          </div>
-        )}
-
         <Input
           placeholder="Grid Target 2"
-          value={secretValue5}
-          onChange={(e) => setSecretValue5(Number(e.target.value))}
+          value={targetNumber2}
+          onChange={(e) => setTargetNumber2(Number(e.target.value))}
         />
-         <Input
+        <Input
           placeholder="Grid Value 2"
-          value={secretValue6}
-          onChange={(e) => setSecretValue6(Number(e.target.value))}
+          value={targetValue2}
+          onChange={(e) => setTargetValue2(Number(e.target.value))}
+        />
+        <Input
+          placeholder="Grid Target 3"
+          value={targetNumber3}
+          onChange={(e) => setTargetNumber3(Number(e.target.value))}
+        />
+        <Input
+          placeholder="Grid Value 3"
+          value={targetValue3}
+          onChange={(e) => setTargetValue3(Number(e.target.value))}
+        />
+        <Input
+          placeholder="Grid Target 4"
+          value={targetNumber4}
+          onChange={(e) => setTargetNumber4(Number(e.target.value))}
+        />
+        <Input
+          placeholder="Grid Value 4"
+          value={targetValue4}
+          onChange={(e) => setTargetValue4(Number(e.target.value))}
+        />
+        <Input
+          placeholder="Grid Target 5"
+          value={targetNumber5}
+          onChange={(e) => setTargetNumber5(Number(e.target.value))}
+        />
+        <Input
+          placeholder="Grid Value 5"
+          value={targetValue5}
+          onChange={(e) => setTargetValue5(Number(e.target.value))}
+        />
+        <Input
+          placeholder="Grid Target 6"
+          value={targetNumber6}
+          onChange={(e) => setTargetNumber6(Number(e.target.value))}
+        />
+        <Input
+          placeholder="Grid Value 6"
+          value={targetValue6}
+          onChange={(e) => setTargetValue6(Number(e.target.value))}
+        />
+        <Input
+          placeholder="Grid Target 7"
+          value={targetNumber7}
+          onChange={(e) => setTargetNumber7(Number(e.target.value))}
+        />
+        <Input
+          placeholder="Grid Value 7"
+          value={targetValue7}
+          onChange={(e) => setTargetValue7(Number(e.target.value))}
+        />
+        <Input
+          placeholder="Grid Target 8"
+          value={targetNumber8}
+          onChange={(e) => setTargetNumber8(Number(e.target.value))}
+        />
+        <Input
+          placeholder="Grid Value 8"
+          value={targetValue8}
+          onChange={(e) => setTargetValue8(Number(e.target.value))}
+        />
+        <Input
+          placeholder="Grid Target 9"
+          value={targetNumber9}
+          onChange={(e) => setTargetNumber9(Number(e.target.value))}
+        />
+        <Input
+          placeholder="Grid Value 9"
+          value={targetValue9}
+          onChange={(e) => setTargetValue9(Number(e.target.value))}
+        />
+        <Input
+          placeholder="Grid Target 10"
+          value={targetNumber10}
+          onChange={(e) => setTargetNumber10(Number(e.target.value))}
+        />
+        <Input
+          placeholder="Grid Value 10"
+          value={targetValue10}
+          onChange={(e) => setTargetValue10(Number(e.target.value))}
         />
       </div>
 
